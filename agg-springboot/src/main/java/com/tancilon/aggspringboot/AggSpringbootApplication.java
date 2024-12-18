0f4a80631f2267ece427df8bd1c86656af1b5300
邓奇 <2043688237@qq.com>
2024-12-18 16:30:28 +0800

    初始化后端项目

---
+++ agg-springboot/src/main/java/com/tancilon/aggspringboot/AggSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tancilon.aggspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggSpringbootApplication.class, args);
+    }
+
+}
---
+++ agg-springboot/src/test/java/com/tancilon/aggspringboot/AggSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tancilon.aggspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
