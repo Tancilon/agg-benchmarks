22d21cee1b8be60c389583353099cc9afff7830d
邓奇 <2043688237@qq.com>
2024-12-18 16:23:01 +0800

    初始化后端项目

---
+++ src/main/java/com/tancilon/aggbenchmarks/AggBenchmarksApplication.java
@@ -0,0 +1,13 @@
+package com.tancilon.aggbenchmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggBenchmarksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggBenchmarksApplication.class, args);
+    }
+
+}
