37e53e8bc745963ae5ecaf268e3c674ea3c8adac
邓奇 <2043688237@qq.com>
2025-02-25 21:13:37 +0800

    初始化项目

--- agg-springboot/src/main/java/com/tancilon/aggspringboot/AggSpringbootApplication.java
+++ agg-springboot/src/main/java/com/tancilon/aggspringboot/AggSpringbootApplication.java
@@ -1,9 +1,11 @@
 package com.tancilon.aggspringboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tancilon.aggspringboot.mapper")
 public class AggSpringbootApplication {
 
     public static void main(String[] args) {
--- agg-springboot/src/main/java/com/tancilon/aggspringboot/controller/AlgorithmsController.java
+++ agg-springboot/src/main/java/com/tancilon/aggspringboot/controller/AlgorithmsController.java
@@ -1,4 +1,21 @@
 package com.tancilon.aggspringboot.controller;
 
+import com.tancilon.aggspringboot.mapper.AlgorithmsMapper;
+import com.tancilon.aggspringboot.pojo.Algorithms;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AlgorithmsController {
+    @Resource
+    private AlgorithmsMapper algorithmsMapper;
+
+    // RequestBody 能接收传输过来的json对象
+    @PostMapping("algorithms/create")
+    public String create(@RequestBody Algorithms algorithm) {
+        algorithmsMapper.insertSelective(algorithm);
+        return algorithmsMapper.selectByPrimaryKey(1).toString();
+    }
 }
